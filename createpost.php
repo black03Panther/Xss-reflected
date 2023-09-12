9994d30eb68d25400eb4744c4685cb1bef9ddd75
black03Panther <henning.jeschke@urs-os.de>
2023-09-12 21:01:14 +0200

    Updates Leute Updates

--- createpost.php
+++ createpost.php
@@ -22,7 +22,7 @@ $loggedInUsername = getLoggedInUsername();
     $sql = "SELECT * FROM user";
     $result = mysqli_query($db, $sql);
     ?>
-
+    
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
--- index.php
+++ index.php
@@ -15,15 +15,10 @@ $loggedInUsername = getLoggedInUsername();
 
   $names = ["Leslie Alexander", "John Doe", "Jane Smith", "Michael Johnson"];
   $emails = ["leslie.alexander@example.com", "john.doe@example.com", "jane.smith@example.com", "michael.johnson@example.com"];
+  $Jobs = ["Front-end Developer", "Back-end Developer", "UI/UX Designer", "Product Manager", "Data Analyst"];
+  $Times = ["1", "2", "3", "4", "5"];
 
-  $randomIndex = array_rand($names);
-  $randomName = $names[$randomIndex];
-  $randomEmail = $emails[$randomIndex];
-  $randomJob = $Jobs[$randomIndex];
-  $randomTime = $Times[$randomIndex];
 
-  $Jobs = ["Front-end Developer"];
-  $Times = ["1", "2", "3", "4"];
 
   $images = [
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
@@ -582,10 +577,17 @@ $loggedInUsername = getLoggedInUsername();
           Some of our Member
         </h2>
         <ul role="list" class="divide-y divide-gray-100">
-        <?php for($i=0;$i <6; $i++)  { ?>
+        <?php for ($i = 0; $i < 6; $i++) { 
+    $randomIndex = array_rand($names);
+    $randomName = $names[$randomIndex];
+    $randomEmail = $emails[$randomIndex];
+    $randomJob = $Jobs[array_rand($Jobs)]; 
+    $randomTime = $Times[$randomIndex];
+    $randomImage = $images[array_rand($images)]; 
+?>
   <li class="flex justify-between gap-x-6 py-5">
     <div class="flex min-w-0 gap-x-4">
-      <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="c echo $randomImage; ?>" alt="">
+    <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="<?php echo $randomImage; ?>" alt="">
       <div class="min-w-0 flex-auto">
         <p class="text-sm font-semibold leading-6 text-gray-900"><?php echo $randomName; ?></p>
         <p class="mt-1 truncate text-xs leading-5 text-gray-500"><?php echo $randomEmail; ?></p>
